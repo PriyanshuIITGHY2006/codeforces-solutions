2c5caa024c2a41d77b12db89f8cd531788c13d7a
PriyanshuIITGHY2006 <priyanshuib01@gmail.com>
2026-06-17 20:12:23 +0000

    sync: update archive [2026-06-17]

--- problems/1400/2190B1-Sub-RBS-Easy-Version/README.md
+++ problems/1400/2190B1-Sub-RBS-Easy-Version/README.md
@@ -10,4 +10,4 @@
 
 **Language:** C++23 (GCC 14-64, msys2)
 
-**Submission:** [Link](https://codeforces.com/contest/2190/submission/379257861)
+**Submission:** [Link](https://codeforces.com/contest/2190/submission/379265277)
--- problems/1400/2190B1-Sub-RBS-Easy-Version/solution.cpp
+++ problems/1400/2190B1-Sub-RBS-Easy-Version/solution.cpp
@@ -1,4 +1,4 @@
 // Problem: 2190B1 - Sub-RBS (Easy Version)
-// Submission: https://codeforces.com/contest/2190/submission/379257861
+// Submission: https://codeforces.com/contest/2190/submission/379265277
 //
 // Drop 2190B1.cpp into the solutions/ folder and push to add your code here.
